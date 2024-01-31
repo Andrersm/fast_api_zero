f2a1fcf14321fd3928f2bea021f5813928c4084a
andrersm <andre.mv.2@hotmail.com>
2024-01-31 18:39:24 -0300

    Protege os endpoints PUT e DELETE com autenticação

--- tests/test_app.py
+++ tests/test_app.py
@@ -26,35 +26,16 @@ def test_create_user(client):
     }
 
 
-def test_create_user_exists(client):
-    response = client.post(
-        '/users',
-        json={
-            'username': 'alice',
-            'email': 'alice@example.com',
-            'password': 'secret',
-        },
-    )
-    response = client.post(
-        '/users',
-        json={
-            'username': 'alice',
-            'email': 'alice@example.com',
-            'password': 'secret',
-        },
-    )
-    assert response.status_code == 400
-
-
 def test_read_users(client):
     response = client.get('/users')
     assert response.status_code == 200
     assert response.json() == {'users': []}
 
 
-def test_update_user(client, user):
+def test_update_user(client, user, token):
     response = client.put(
-        '/users/1',
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'bob',
             'email': 'bob@example.com',
@@ -69,30 +50,28 @@ def test_update_user(client, user):
     }
 
 
-def test_update_user_not_found(client):
-    response = client.put(
-        '/users/10',
-        json={
-            'username': 'bob',
-            'email': 'bob@example.com',
-            'password': 'mynewpassword',
-        },
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
     )
-    assert response.status_code == 404
-
-
-def test_delete_user(client, user):
-    response = client.delete('/users/1')
     assert response.status_code == 200
     assert response.json() == {'detail': 'User deleted'}
-
-
-def test_delete_user_not_found(client):
-    response = client.delete('/users/10')
-    assert response.status_code == 404
 
 
 def test_read_users_with_users(client, user):
     user_schema = UserPublic.model_validate(user).model_dump()
     response = client.get('/users/')
     assert response.json() == {'users': [user_schema]}
+
+
+def test_get_token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    token = response.json()
+
+    assert response.status_code == 200
+    assert 'access_token' in token
+    assert 'token_type' in token
